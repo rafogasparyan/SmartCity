528c925f541ec721e41c384756ffdd270a207c7f
Vahe <vahe0512@gmail.com>
2025-03-29 17:44:04 +0400

    Add C# project skeleton

---
+++ CSharpServices/RouteOptimizationService/Program.cs
@@ -0,0 +1,16 @@
+﻿using Confluent.Kafka;
+using System;
+public class Program {
+    public static void Main(string[] args) {
+        var consumerConfig = new ConsumerConfig {
+            BootstrapServers = Environment.GetEnvironmentVariable("KAFKA_BOOTSTRAP_SERVERS"),
+            GroupId = "route-optimizer",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<string, byte[]>(consumerConfig).Build();
+        consumer.Subscribe("gps_data");
+        
+        // Add Google Roads API integration here
+    }
+}
